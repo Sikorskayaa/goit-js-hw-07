d875efa4196b8e2c58cd7a1a1e3a32fd71d899b4
Sikorskaya270797 <irasikorskaya27@icloud.com>
2023-09-19 13:29:38 +0300

    push3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,4 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 console.log(galleryItems);
 
@@ -12,65 +11,37 @@ function createMarkup(arr) {
   return arr
     .map(
       (item) => `
-  <li class"gallery__item js-gallery-item">
-   <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="large-image.jpg"
-      alt="${item.description}"
-    />
-  </a>
-  </li>
-    `
+    <li class="gallery__item js-gallery-item">
+      <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>
+      `
     )
     .join("");
 }
 
-// import { galleryItems } from "./gallery-items.js";
+function handleClick(event) {
+  event.preventDefault();
 
-// console.log(galleryItems);
+  if (event.target === event.currentTarget) {
+    return;
+  }
 
-// const list = document.querySelector(".gallery");
+  const currentGallery = event.target.closest(".js-gallery-item");
+  if (!currentGallery) return;
 
-// list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
-// list.addEventListener("click", handleClick);
-
-// function createMarkup(arr) {
-//   return arr
-//     .map(
-//       (item) => `
-//     <li class="gallery__item js-gallery-item">
-//       <a class="gallery__link" href="${item.original}">
-//         <img
-//           class="gallery__image"
-//           src="${item.preview}"
-//           data-source="${item.original}"
-//           alt="${item.description}"
-//         />
-//       </a>
-//     </li>
-//       `
-//     )
-//     .join("");
-// }
-
-// function handleClick(event) {
-//   event.preventDefault();
-
-//   if (event.target === event.currentTarget) {
-//     return;
-//   }
-
-//   const currentGallery = event.target.closest(".js-gallery-item");
-//   if (!currentGallery) return;
-
-//   const instance = basicLightbox.create(`
-//     <img
-//       class="gallery__image"
-//       src="${event.target.dataset.source}"
-//       alt="${event.target.alt}"
-//     />
-//   `);
-//   instance.show();
-// }
+  const instance = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src="${event.target.dataset.source}"
+      alt="${event.target.alt}"
+    />
+  `);
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(
+      (item) => `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${item.original}">
+                    <img
+                        class="gallery__image"
+                        src="${item.preview}"
+                        alt="${item.description}"
+                        title="${item.description}"
+                    />
+                </a>
+            </li>
+            `
+    )
+    .join("");
+}
+
+const list = document.querySelector(".gallery");
+list.innerHTML = createMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "title",
+  captionDelay: 250,
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    lightbox.open();
+  }
+});
